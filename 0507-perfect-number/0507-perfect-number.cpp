c8b139b7cfca34c1349b4e61c713352459587714
Pydeti Yeswanth Sai RaghuRam <119839691+yeswanthsairaghuram@users.noreply.github.com>
2025-07-08 21:18:19 +0530

    Time: 1034 ms (52.28%), Space: 7.6 MB (89.29%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-       vector<int> res;
-       for(int i=1;i<num;i++){
+    int sum=0;
+       for(int i=1;i<=num/2;i++){
         if(num%i==0){
-            res.push_back(i);
+            sum+=i;
         }
        } 
-       int sum=accumulate(res.begin(), res.end(), 0);
        if(sum == num) return true;
-       else return false;
+       return false;
     }
 };
