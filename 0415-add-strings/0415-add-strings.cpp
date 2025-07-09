38c19a4bd7639ab6da30585af7fc531789c1a17c
Pydeti Yeswanth Sai RaghuRam <119839691+yeswanthsairaghuram@users.noreply.github.com>
2025-07-09 22:21:27 +0530

    Time: 0 ms (100%), Space: 9 MB (95.2%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string result = "";
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int digit1 = (i >= 0) ? num1[i--] - '0' : 0;
+            int digit2 = (j >= 0) ? num2[j--] - '0' : 0;
+            int sum = digit1 + digit2 + carry;
+            result.push_back((sum % 10) + '0');
+            carry = sum / 10;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
